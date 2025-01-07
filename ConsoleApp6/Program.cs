5a00d07f54b04e65599cf384a49ce708fe435bb7
Paula Carolina Silva <paulacarolinasilva@Paulas-MacBook-Air.local>
2025-01-07 12:40:20 +0000

    Added random number generation

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -10,6 +10,12 @@ namespace ConsoleApp6
   internal class Program
   {
     static void Main(string[] args)
-    {}
+    {
+      Random rng = new Random();
+
+      int num1 = rng.Next(1, 11);
+      int num2 = rng.Next(1, 11);
+
+    }
   }
 }
